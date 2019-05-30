b167a2c4f275f5c5b7d5985dea40b715d7c82e3c
ladis8 <stefka885@gmail.com>
2019-05-30 08:33:58 +0200

    L073_fork_hotfix

--- Core/inc/stm32l0xx_hw_conf.h
+++ Core/inc/stm32l0xx_hw_conf.h
@@ -8,68 +8,57 @@
  * \author		MCD Application Team, Ladislav Stefka
  *
  */
-
-#ifndef __HW_CONF_L0_H__
-#define __HW_CONF_L0_H__
+#ifndef __HW_CONF_L073_H__
+#define __HW_CONF_L073_H__
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
- //USED PINS
-  //LoRa Shield SPI PA0, PA6, PA7, PA5, PB0
-  //LoRa Shield DIO PB3, PB4, PB5, PB6
-  //LoRa Shield Antenna PC1
-  //ADC PA4
-  //USART PA2, PA3
-  //I2C PA9,PA10
-  //PWM PA1
+
+//LORA I/O definition
+#define RADIO_RESET_PORT                          GPIOA
+#define RADIO_RESET_PIN                           GPIO_PIN_0
+
+#define RADIO_MOSI_PORT                           GPIOA
+#define RADIO_MOSI_PIN                            GPIO_PIN_7
+
+#define RADIO_MISO_PORT                           GPIOA
+#define RADIO_MISO_PIN                            GPIO_PIN_6
+
+#define RADIO_SCLK_PORT                           GPIOA
+#define RADIO_SCLK_PIN                            GPIO_PIN_5
+
+#define RADIO_NSS_PORT                            GPIOB
+#define RADIO_NSS_PIN                             GPIO_PIN_6
+
+#define RADIO_DIO_0_PORT                          GPIOA
+#define RADIO_DIO_0_PIN                           GPIO_PIN_10
+
+#define RADIO_DIO_1_PORT                          GPIOB
+#define RADIO_DIO_1_PIN                           GPIO_PIN_3
+
+#define RADIO_DIO_2_PORT                          GPIOB
+#define RADIO_DIO_2_PIN                           GPIO_PIN_5
+
+#define RADIO_DIO_3_PORT                          GPIOB
+#define RADIO_DIO_3_PIN                           GPIO_PIN_4
 
 
-
- //LORA I/O definition
- #define RADIO_RESET_PORT                          GPIOA
- #define RADIO_RESET_PIN                           GPIO_PIN_0
-
- #define RADIO_MOSI_PORT                           GPIOA
- #define RADIO_MOSI_PIN                            GPIO_PIN_7
-
- #define RADIO_MISO_PORT                           GPIOA
- #define RADIO_MISO_PIN                            GPIO_PIN_6
-
- #define RADIO_SCLK_PORT                           GPIOA
- #define RADIO_SCLK_PIN                            GPIO_PIN_5
-
- #define RADIO_NSS_PORT                            GPIOB
- #define RADIO_NSS_PIN                             GPIO_PIN_0
-
- #define RADIO_DIO_0_PORT                          GPIOB
- #define RADIO_DIO_0_PIN                           GPIO_PIN_3
-
- #define RADIO_DIO_1_PORT                          GPIOB
- #define RADIO_DIO_1_PIN                           GPIO_PIN_4
-
- #define RADIO_DIO_2_PORT                          GPIOB
- #define RADIO_DIO_2_PIN                           GPIO_PIN_5
-
- #define RADIO_DIO_3_PORT                          GPIOB
- #define RADIO_DIO_3_PIN                           GPIO_PIN_6
+#define RADIO_ANT_SWITCH_PORT                     GPIOC
+#define RADIO_ANT_SWITCH_PIN                      GPIO_PIN_1
 
 
- #define RADIO_ANT_SWITCH_PORT                     GPIOC
- #define RADIO_ANT_SWITCH_PIN                      GPIO_PIN_1
+ //I2C definitions
+#define I2Cx									  I2C1
+#define I2Cx_GPIOPORT							  GPIOB
+#define I2Cx_GPIO_CLK_ENABLE()					  __GPIOB_CLK_ENABLE()
 
+#define I2Cx_SCL_PIN							  GPIO_PIN_8
+#define I2Cx_SDA_PIN 							  GPIO_PIN_9
 
-  //I2C definitions
- #define I2Cx									  I2C1
- #define I2Cx_GPIOPORT							  GPIOA
- #define I2Cx_GPIO_CLK_ENABLE()					  __GPIOA_CLK_ENABLE()
-
- #define I2Cx_SCL_PIN							  GPIO_PIN_9
- #define I2Cx_SDA_PIN 							  GPIO_PIN_10
-
- #define I2Cx_CLK_ENABLE()                		  __HAL_RCC_I2C1_CLK_ENABLE()
- #define I2Cx_AF								 GPIO_AF6_I2C1
+#define I2Cx_CLK_ENABLE()                		  __HAL_RCC_I2C1_CLK_ENABLE()
+#define I2Cx_AF									  GPIO_AF4_I2C1
 
 
  /*  SPI MACRO redefinition */
@@ -163,4 +152,3 @@
 #endif
 
 #endif /* __HW_CONF_L0_H__ */
-
